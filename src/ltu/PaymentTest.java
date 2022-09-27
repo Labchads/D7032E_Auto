a6ab9a5320544ab08e2872cb850aab283473df3f
LeifTeorin <hjalle2000@gmail.com>
2022-09-27 16:28:24 +0200

    buncha tester gjorda

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -20,8 +20,87 @@ public class PaymentTest
     @Test
     public void testAge() throws IOException{
         PaymentImpl payment = new PaymentImpl(getCalendar());
-        int loan = payment.getMonthlyAmount("20100327", 10000, 50, 50);
-        assertTrue(loan==0);
+        int loan = payment.getMonthlyAmount("2003032756735", 0, 100, 100);
+        assertEquals(0, loan);
+    }
+    
+    @Test
+    public void testAge2() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        int loan = payment.getMonthlyAmount("1965032756735", 0, 100, 100);
+        assertEquals(0, loan);
+    }
+    
+    @Test
+    public void testAge3() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        int loan = payment.getMonthlyAmount("1975032756735", 0, 100, 100);
+        assertEquals(2816, loan);
+    }
+    
+    @Test
+    public void testHalfTime() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        int loan = payment.getMonthlyAmount("2000032756735", 0, 49, 100);
+        int loan2 = payment.getMonthlyAmount("2000032756735", 0, 51, 100);
+        assertTrue(loan != loan2);
+    }
+    
+    @Test
+    public void testNotFullTime() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+//        int loan = payment.getMonthlyAmount("2000032756735", 0, 100, 100);
+        int loan2 = payment.getMonthlyAmount("2000032756735", 0, 99, 100);
+        assertEquals(loan2, 1396+3564);
+    }
+    
+    @Test
+    public void testFullTime() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        int loan = payment.getMonthlyAmount("2000032756735", 0, 100, 100);
+        assertTrue(loan == 7088 + 2816);
+    }
+    
+    @Test
+    public void test501() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        int loan = payment.getMonthlyAmount("2000032756735", 0, 100, 100);
+        assertTrue(loan == 7088 + 2816);
+    }
+    
+    @Test
+    public void test502() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        int loan = payment.getMonthlyAmount("2000032756735", 0, 100, 100);
+        assertTrue(loan == 7088 + 2816);
+    }
+    
+    @Test
+    public void test503() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        int loan = payment.getMonthlyAmount("2000032756735", 0, 50, 100);
+        assertTrue(loan == 3564 + 1396);
+    }
+    
+    @Test
+    public void test504() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        int loan = payment.getMonthlyAmount("2000032756735", 0, 50, 100);
+        assertTrue(loan == 3564 + 1396);
+    }
+    
+    @Test
+    public void test505() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        int loan = payment.getMonthlyAmount("2000032756735", 0, 100, 100);
+        assertTrue(loan == 7088 + 2816);
+    }
+    
+    @Test
+    public void test506() throws IOException{
+    	PaymentImpl payment = new PaymentImpl(getCalendar());
+        String cash = payment.getNextPaymentDay();
+        assertTrue(cash == "20220930");
     }
     /*public static void main(String[] args){
         testSilly();
